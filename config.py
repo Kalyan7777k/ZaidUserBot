7cef842c0ff07e44b8b2db8d190f483e8e3cc9f1
Kalyan7777k <125959457+Kalyan7777k@users.noreply.github.com>
2023-02-28 14:39:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,14 +14,14 @@ OWNER_ID = int(getenv("OWNER_ID",  "5655356960"))
 MONGO_URL = getenv("MONGO_URL",  "mongodb+srv://babukalyan977:babukalyan977@cluster0.fvkyo4r.mongodb.net/?retryWrites=true&w=majority")
 BOT_TOKEN = getenv("BOT_TOKEN", "6266931054:AAEkvXmVTL7wMLlgjwBCq1_aXgpG0kg6Q3g")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/3c52a01057865f7511168.jpg')
-ALIVE_TEXT = getenv("ALIVE_TEXT")
+ALIVE_TEXT = getenv("ALIVE_TEXT",  "kalyan babu")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "BQBiMZkAJ5H78ZkPI2dDFrytTzyGzA3st65zNWVwuwlNuP0GpnJp5peI96MrRrUUgJOpoSWoDBJoPpIDo2euS6AUZyVEs4qUbecQa0vr3OIWMLTYsYucolK27B09WIuH3ICTor6hddZhijHh5H3hUAhmpDGHRV8eZToNMYQLpk9w-n3iHVKTDhb8Scld4FISykyEHbsrleOrBlYbOQzUsnLTBS8mOQYJej2edqGNam88afE2tumIPHCnt096mYl-pECt1q-7N0HgCXoYlU7bQbYCAwCUKRK0t8HZ2pFsqm__aXtfdJhYoWksUG3r0vz5s2wonT6-wJ2tdPC4ftTQUTr_ppqVQgAAAAFRFeYgAA")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQBiMZkAAtbZW40936n6Eb93nFHuu0Pt-NKrxnFDrHk98M5hnkEGa3onOkpmtAqCEkEbxtPZnCroW_SpRDjRpT42jSKSFm5rpvs6E-AitjbAlKu2X_6aebjoj60iSc9taCVPCbw-9NV6_4Xb-DQxWJblY9JthmWQkI2mIgGxpNxhtmZED0Ps3bVLLmGBcDLsiQtYIX8S2Slap453vM23jP1nXyfNelF6xVdAHmFRVN7mT1MNCoh_aEE8ANgbLpGzqvFKbf6U0_diMtbRrDGoMOFu7luq--WDIONWmWpAbQnaZSD4_TJR332gp2zkBaDodbFtHM7hw3tGKOICdYtX0VOdnD-LiQAAAAFRFeYgAA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
